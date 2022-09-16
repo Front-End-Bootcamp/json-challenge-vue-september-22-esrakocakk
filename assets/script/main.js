9b52de7cf4ce1f2a64532608af4c9d243d6a1028
Esra Koçak <esrakocak470@gmail.com>
2022-09-16 23:06:05 +0300

    uploaded homework1

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,332 @@
-console.log('👻');
+const users = [
+	{
+		"name": "Abdulkadir Çelebi",
+		"id": "631db8d58b3d1cc9a2144cf5",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Abdullah Kuş",
+		"id": "631db8d5e9d3a5f66526a610",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Ali̇can Berat Tutumoğlu",
+		"id": "631db8d5ccd6352cc1714fe1",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Alihan Değirmenciler",
+		"id": "631db8d5f2f01a8fa3a23a21",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Altuğ Altay",
+		"id": "631db8d59fc2797f8369ce4b",
+		"group": "IndianRed",
+		"type": null
+	},
+	{
+		"name": "Anıl Sezer",
+		"id": "631db8d5e4bd98e9a2934be8",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Aybüke Polat",
+		"id": "631db8d551b762e7e9a92f75",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Emircan Demir",
+		"id": "631db8d5f3c680c55c6cdb0a",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Emircan Özenir",
+		"id": "631db8d51f69ce4d2b6e43ea",
+		"group": "YellowGreen",
+		"type": null
+	},
+	{
+		"name": "Engin İnsel",
+		"id": "631db8d566fd25d2034e2f26",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Esra Koçak",
+		"id": "631db8d5164158e25610e4c1",
+		"group": "IndianRed",
+		"type": null
+	},
+	{
+		"name": "Fatih Tarım",
+		"id": "631db8d5ce015ffe1b934ade",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Fatih Tufan",
+		"id": "631db8d5e23e63fd79581954",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Feruze Beyza Başer",
+		"id": "631db8d5ce659b4ea9500f13",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Furkan Akin",
+		"id": "631db8d51b06709289c62227",
+		"group": "LightSeaGreen",
+		"type": null
+	},
+	{
+		"name": "Gorkem Sakalak",
+		"id": "631db8d5d1bfff434fffbe16",
+		"group": "YellowGreen",
+		"type": null
+	},
+	{
+		"name": "İlker Durmaz",
+		"id": "631db8d5afc25569fabd9cfb",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Irina Ivanova",
+		"id": "631db8d5c30b2de5a8096e81",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "İsa Sarı",
+		"id": "631db8d5a14b0c131b273d39",
+		"group": "IndianRed",
+		"type": null
+	},
+	{
+		"name": "İsmet Erkin Yavuzlar",
+		"id": "631db8d538130a4d8379e876",
+		"group": "IndianRed",
+		"type": null
+	},
+	{
+		"name": "Kaplan Arkan",
+		"id": "631db8d52e9d315ff36d861c",
+		"group": "IndianRed",
+		"type": null
+	},
+	{
+		"name": "Mahmut Çapkın",
+		"id": "631db8d5fd326c55b14e0041",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Mahmut Hüdai Göl",
+		"id": "631db8d503b36479e39f3e90",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Mehmet Kırsakal",
+		"id": "631db8d5c418ddb40198d2c3",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Onur Çöpoğlu",
+		"id": "631db8d530d1dafd77aec3aa",
+		"group": "YellowGreen",
+		"type": null
+	},
+	{
+		"name": "Recep Emre Gürdrama",
+		"id": "631db8d50c0138167e74672b",
+		"group": "IndianRed",
+		"type": null
+	},
+	{
+		"name": "Samet Keleş",
+		"id": "631db8d5d02e4c9d6b2ea90a",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Seyit Arslantürk",
+		"id": "631db8d56c23487eb3b5a341",
+		"group": "Orchid",
+		"type": null
+	},
+	{
+		"name": "Vahide Erdoğan",
+		"id": "631db8d5d8ec17dbf10f7055",
+		"group": "IndianRed",
+		"type": null
+	},
+	{
+		"name": "Yunus Emre Topçu",
+		"id": "631db8d59ff3c7023cf7ac1a",
+		"group": "SteelBlue",
+		"type": null
+	},
+	{
+		"name": "Hüseyin Can Çirkin",
+		"id": "631db8d5951bff669092e4a5",
+		"group": "LightSeaGreen",
+		"type": "assistant"
+	},
+	{
+		"name": "Eralp Özcan",
+		"id": "631db8d55e3d014e2087b9ac",
+		"group": "IndianRed",
+		"type": "assistant"
+	},
+	{
+		"name": "Turgay Dede",
+		"id": "631db8d5f2338e141e07dc76",
+		"group": "YellowGreen",
+		"type": "assistant"
+	},
+	{
+		"name": "Osman Mahmutçepoğlu",
+		"id": "631db8d5d05cf94db3255dd8",
+		"group": "Orchid",
+		"type": "assistant"
+	},
+	{
+		"name": "Enes Taha Sarı",
+		"id": "631db8d560cfc88b4a6aa200",
+		"group": "SteelBlue",
+		"type": "assistant"
+	}
+]
+
+
+	function getStudents(newGroup){
+		var student=users.filter(x=> x.group==="SteelBlue").filter(y=> y.type==null).map(z=> z.name)
+		return student;
+	}
+
+	function getAssistant(newGroup){
+		var assistant=users.filter(x=> x.group==="SteelBlue").filter(y=> y.type!==null).map(z=> z.name)
+		return assistant;
+	}
+
+	function getGroup1(newGroup){
+		var student=getStudents(newGroup)
+		var assistant=getAssistant(newGroup)
+		var group={
+			"groupName": "SteelBlue",
+			"students":student,
+			"assistant":assistant
+		}
+		return group;
+	}
+	var gr1=getGroup1("SteelBlue");
+	console.log(gr1);
+
+	function getStudents2(newGroup){
+		var student=users.filter(x=> x.group==="Orchid").filter(y=> y.type==null).map(z=> z.name)
+		return student;
+	}
+
+	function getAssistant2(newGroup){
+		var assistant=users.filter(x=> x.group==="Orchid").filter(y=> y.type!==null).map(z=> z.name)
+		return assistant;
+	}
+
+	function getGroup2(newGroup){
+		var student=getStudents2(newGroup)
+		var assistant=getAssistant2(newGroup)
+		var group={
+			"groupName": "Orchid",
+			"students":student,
+			"assistant":assistant
+		}
+		return group;
+	}
+	var gr1=getGroup2("Orchid");
+	console.log(gr1);
+
+
+	function getStudents3(newGroup){
+		var student=users.filter(x=> x.group==="YellowGreen").filter(y=> y.type==null).map(z=> z.name)
+		return student;
+	}
+
+	function getAssistant3(newGroup){
+		var assistant=users.filter(x=> x.group==="YellowGreen").filter(y=> y.type!==null).map(z=> z.name)
+		return assistant;
+	}
+
+	function getGroup3(newGroup){
+		var student=getStudents3(newGroup)
+		var assistant=getAssistant3(newGroup)
+		var group={
+			"groupName": "YellowGreen",
+			"students":student,
+			"assistant":assistant
+		}
+		return group;
+	}
+	var gr1=getGroup3("YellowGreen");
+	console.log(gr1);
+
+	function getStudents4(newGroup){
+		var student=users.filter(x=> x.group==="IndianRed").filter(y=> y.type==null).map(z=> z.name)
+		return student;
+	}
+
+	function getAssistant4(newGroup){
+		var assistant=users.filter(x=> x.group==="IndianRed").filter(y=> y.type!==null).map(z=> z.name)
+		return assistant;
+	}
+
+	function getGroup4(newGroup){
+		var student=getStudents4(newGroup)
+		var assistant=getAssistant4(newGroup)
+		var group={
+			"groupName": "IndianRed",
+			"students":student,
+			"assistant":assistant
+		}
+		return group;
+	}
+	var gr1=getGroup4("IndianRed");
+	console.log(gr1);
+
+	function getStudents5(newGroup){
+		var student=users.filter(x=> x.group==="LightSeaGreen").filter(y=> y.type==null).map(z=> z.name)
+		return student;
+	}
+
+	function getAssistant5(newGroup){
+		var assistant=users.filter(x=> x.group==="LightSeaGreen").filter(y=> y.type!==null).map(z=> z.name)
+		return assistant;
+	}
+
+	function getGroup5(newGroup){
+		var student=getStudents5(newGroup)
+		var assistant=getAssistant5(newGroup)
+		var group={
+			"groupName": "LightSeaGreen",
+			"students":student,
+			"assistant":assistant
+		}
+		return group;
+	}
+	var gr1=getGroup5("LightSeaGreen");
+	console.log(gr1);
+
+ 
+
